0a144ff209451ec5c3624e2b8b6e265374bf28f8
Alexander Pronin <pronin.alx@gmail.com>
2014-04-05 11:51:33 +0300

    First commit

---
+++ RITDravingView.h
@@ -0,0 +1,13 @@
+//
+//  RITDravingView.h
+//  2401DrawingsTestHW
+//
+//  Created by Pronin Alexander on 05.04.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITDravingView : UIView
+
+@end
